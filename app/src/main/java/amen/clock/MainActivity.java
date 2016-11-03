6ddd70a123876091d2b4db13cdc445ca0337e17f
andrewjamen <ajamen@ilstu.edu>
2016-11-03 18:29:24 -0500

    Initial commit

---
+++ app/src/main/java/amen/clock/MainActivity.java
@@ -0,0 +1,15 @@
+package amen.clock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int number =5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

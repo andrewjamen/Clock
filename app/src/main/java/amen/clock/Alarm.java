e9e1c2451242972b045cda1e9a343bd21a6c7e9b
andrewjamen <ajamen@ilstu.edu>
2016-11-08 13:14:52 -0600

    Alarm class constuctor

---
+++ app/src/main/java/amen/clock/Alarm.java
@@ -0,0 +1,38 @@
+package amen.clock;
+
+/**
+ * Created by Andrew on 11/8/2016.
+ */
+import android.location.Location;
+
+
+
+public class Alarm {
+
+    private long time;
+    private String message;
+    private boolean[] rptDay;
+    private int timeZone;
+    private Location location;
+
+
+
+    public Alarm(long time, String message, boolean[] rptDay, int timeZone, Location location){
+
+    }
+
+
+    public Alarm(long time, String message, Location location){
+
+    }
+
+    public Alarm(long time, Location location){
+
+    }
+
+
+
+
+
+
+}
